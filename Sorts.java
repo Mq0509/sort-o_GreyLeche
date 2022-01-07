2bc1525a7ea08895465d24ce9b4029265d6b77a8
May Qiu <90876910+Mq0509@users.noreply.github.com>
2022-01-06 21:40:44 -0500

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -1,66 +1,25 @@
-  import java.util.ArrayList;
-  public class Sorts{
-  public static void bubbleSortV( ArrayList<Comparable> data )
-  {
-    for (int p = 0; p < data.size(); p++) {
-      Comparable temp = 0;
-      for(int i = (data.size()-1); i > 0; i--) {
-        if(data.get(i).compareTo(data.get(i-1)) < 0) {
-          temp = data.get(i);
-          data.set(i, data.get(i-1));
-          data.set(i-1, temp);
-        }
-      }
-    }
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Driver{
+  public static void main(String[] args) {
+    ArrayList a = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    ArrayList b = new ArrayList<>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
+
+    System.out.println("Best case for bubble sort");
+    Sorts.bubble(a);
+    System.out.println("Worst case for bubble sort");
+    Sorts.bubble(b);
+    
+    System.out.println("Best case for selection sort");
+    Sorts.selection(a);
+    System.out.println("Worst case for selection sort");
+    Sorts.selection(b);
+    
+    System.out.println("Best case for insertion sort");
+    Sorts.insertion(a);
+    System.out.println("Worst case for selection sort");
+    Sorts.insertion(b);
   }
-  public static void selectionSortV( ArrayList<Comparable> data )
-    {
-      //note: this version places greatest value at "rightmost" end
-
-      //maxPos will point to position of SELECTION (greatest value)
-      int maxPos;
-
-      for(int pass = data.size() - 1; pass > 0; pass--) {
-        System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
-        maxPos = pass;
-
-        for(int i = 1; i < pass; i++) {
-          if (data.get(i).compareTo(data.get(maxPos)) > 0) {
-            maxPos = i;
-          }
-          System.out.println( "maxPos: " + maxPos );//diag
-          System.out.println( data );//diag
-        }
-        Comparable dummy = data.get(pass);
-        data.set(pass, data.get(maxPos));
-        data.set(maxPos, dummy);
-
-        System.out.println( "after swap: " +  data );//diag
-      }
-    }//end selectionSort
-    public static void insertionSortV( ArrayList<Comparable> data )
-      {
-        for(int partition = 1; partition < data.size(); partition++) {
-          //partition marks first item in unsorted region
-
-          System.out.println( "\npartition: " + partition + "\tdataset:"); //diag
-          System.out.println( data );
-
-          //traverse sorted region from right to left
-          for(int i = partition; i > 0; i--) {
-
-            // "walk" the current item to where it belongs
-            // by swapping adjacent items
-            if (data.get(i).compareTo(data.get(i - 1)) < 0) {
-              System.out.println( "swap indices "+(i-1)+" & "+i+"..." ); //diag
-              Comparable dummy = data.get(i);
-              data.set(i, data.get(i - 1));
-              data.set(i - 1, dummy);
-            }
-            else {
-              break;
-            }
-          }
-        }
-      }//end insertionSortV
 }
+
